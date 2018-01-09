9cda4e08964e29422a3cc45d22b77e1366d47c12
Gabriele Vassallo <gabriele.vassallo@runtastic.com>
2018-01-09 17:22:34 +0100

    Delete remote branch when option is specified to the push method

--- lib/git/lib.rb
+++ lib/git/lib.rb
@@ -737,6 +737,7 @@ module Git
 
       arr_opts = []
       arr_opts << '--force'  if opts[:force] || opts[:f]
+      arr_opts << '-d' if opts[:delete]
       arr_opts << remote
 
       command('push', arr_opts + [branch])
